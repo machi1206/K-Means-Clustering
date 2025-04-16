cf5bc5c2d3a64b59e564a38c64cab916814a3b5e
Shanmukh Machiraju <machiraju.shanmukh@gmail.com>
2025-04-17 05:14:10 +0530

    Add files via upload
    Workflow of program added

---
+++ main.py
@@ -0,0 +1,19 @@
+# import stuff here
+from src.K_Means import Point, KMeans
+from pre_process import load_and_filter
+from plot import plot_clusters_with_map, elbow_plot, silhouette_plot
+
+geojson_file = 'src/districts.json'
+
+telangana_data = load_and_filter(geojson_file)
+
+dataset = [Point(row['Latitude'], row['Longitude']) for _, row in telangana_data.iterrows()]
+
+kmeans = KMeans(k=3, change_threshold=0.001, max_iterations=5000, dataset=dataset)
+centroids = kmeans.workflow()
+
+plot_clusters_with_map(dataset, centroids, india_geojson=geojson_file)
+
+elbow_plot(telangana_data)
+
+silhouette_plot(telangana_data)
